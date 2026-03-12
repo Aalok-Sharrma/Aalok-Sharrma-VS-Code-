85be5fb5efd5f77178670d2d59ab4dd097f3b7a7
Aalok Sharrma <aalokkumarsharrma@gmail.com>
2026-03-12 23:03:39 +0530

    Practiced_FLOYD_S_Triangle

---
+++ Try any code ( Rough )/FLOYD_S_Triangle.java
@@ -0,0 +1,19 @@
+public class FLOYD_S_Triangle {
+
+    public static void FLOYD_S_Triangle(int n){
+        int a=1;
+        for(int i=1; i<=n; i++){ //outer loop for line
+            for(int j=1; j<=i; j++){ //inner loop for printing pattern
+                System.out.print(a+" ");
+                a++;
+            }
+            System.out.println(); //for next line
+        }
+    }
+    public static void main(String[] args) {
+        
+        FLOYD_S_Triangle(5); //function/method calling
+        
+    }
+
+}
