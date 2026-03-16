b7f552145db6ac0d1ec4a95bcb362269efc2b3df
Aalok Sharrma <aalokkumarsharrma@gmail.com>
2026-03-16 21:52:41 +0530

    Coded in Special_AalokSharrma_Pattern_Question file

--- Try any code ( Rough )/Special_AalokSharrma_Pattern_Question.java
+++ Try any code ( Rough )/Special_AalokSharrma_Pattern_Question.java
@@ -1,3 +1,35 @@
 public class Special_AalokSharrma_Pattern_Question {
     
+    public static void pattern(int n){
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if((i==j)||(i==3 && j==1)||(i==4 && j==2)){
+                    System.out.print(" * ");
+                } else{
+                    System.out.print("  ");
+                }
+            } 
+            System.out.println();
+        }
+        
+        int x=n-1;
+        for(int i=1; i<=n-1; i++){
+            for(int j=1; j<=x; j++){
+                if((i==j)||(i==1 && j==3)||(i==3 && j==1)){
+                    System.out.print(" * ");
+                } else{
+                    System.out.print("  ");
+                }
+            } 
+            x--;
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args){
+        
+        pattern(4);
+        
+    }
 }
