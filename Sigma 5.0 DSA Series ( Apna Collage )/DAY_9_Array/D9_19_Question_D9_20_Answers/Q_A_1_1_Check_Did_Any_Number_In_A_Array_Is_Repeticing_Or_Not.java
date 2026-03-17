2dd7cde965b6fe8ef6477296d22fe248f8d9ee7f
Aalok Sharrma <aalokkumarsharrma@gmail.com>
2026-03-17 21:38:24 +0530

    Created My Code Q_A_1_1_Check_Did_Any_Number_In_A_Array_Is_Repeticing_Or_Not

---
+++ Sigma 5.0 DSA Series ( Apna Collage )/DAY_9_Array/D9_19_Question_D9_20_Answers/Q_A_1_1_Check_Did_Any_Number_In_A_Array_Is_Repeticing_Or_Not.java
@@ -0,0 +1,27 @@
+package DAY_9_Array.D9_19_Question_D9_20_Answers;
+
+public class Q_A_1_1_Check_Did_Any_Number_In_A_Array_Is_Repeticing_Or_Not {
+
+    // It is a Shradha Kapara Ma'am and My Code For File Name.
+    
+    public static boolean DidAnyNumberRepetedInArray(int a[]){
+        for(int i=0; i<a.length; i++){
+            for(int j=i+1; j<a.length; j++){
+                if(a[i]==a[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] a={1,2,3,1,6};
+        boolean b=DidAnyNumberRepetedInArray(a);
+        if(b == true){
+        System.out.println("Yes in array a[] numbers repets");
+        }
+        else{
+        System.out.println("No in array a[] No number repets");
+        }
+    }
+}
