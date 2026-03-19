2afd48f691f41f7885f6dbe6691391f8b24a0b80
Aalok Sharrma <aalokkumarsharrma@gmail.com>
2026-03-19 22:40:12 +0530

    Created D10_1_What_Is_Sorting

---
+++ Sigma 5.0 DSA Series ( Apna Collage )/DAY_10_Basic_Sorting_Algorithums/D10_1_What_Is_Sorting.java
@@ -0,0 +1,16 @@
+package DAY_10_Basic_Sorting_Algorithums;
+
+public class D10_1_What_Is_Sorting {
+    
+    /*
+       Sorting -- It is a Arrangement of anythink in an order. (It can be Asending or Decending)
+
+       BASIC SORTING ALGORITHMS
+
+         (1) Bubble Sort
+         (2) Selection Sort
+         (3) Insertion Sort
+         (4) Counting Sort (Advanced Sorting Algorithm)
+         
+    */
+}
