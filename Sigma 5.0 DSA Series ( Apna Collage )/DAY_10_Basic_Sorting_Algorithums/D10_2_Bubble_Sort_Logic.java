a23598315679605f5af684357add01c6f1258fe5
Aalok Sharrma <aalokkumarsharrma@gmail.com>
2026-03-31 07:52:23 +0530

    D10_2_Bubble_Sort_Logic

---
+++ Sigma 5.0 DSA Series ( Apna Collage )/DAY_10_Basic_Sorting_Algorithums/D10_2_Bubble_Sort_Logic.java
@@ -0,0 +1,34 @@
+package DAY_10_Basic_Sorting_Algorithums;
+
+public class D10_2_Bubble_Sort_Logic {
+    
+    /* 
+    
+    Bubble Sort IDEA :- Large elements come to the end of array by swapping with adjacent elements.
+
+       --> Here in bubble sort we swap the elements one by one form i=0 to i=array.length-2;
+
+----------------------------------------- LOGIC START -----------------------------------------
+           for(i=0 to n-2){
+             for(j=0 to n-2-i){
+                FOR i=0
+                   j=5-2-0 = 3(index number)
+
+                FOR i=1
+                   j=5-2-1 = 2(index number)
+
+                FOR i=2
+                   j=5-2-2 = 1(index number)
+
+                FOR i=3
+                   j=5-2-3 = 0(index number)
+             }
+           }
+
+------------------------------------------ LOGIC END ------------------------------------------
+
+       --> Time Complexity = O(n^2)   Not Good Time Complexity **
+
+    
+    */
+}
